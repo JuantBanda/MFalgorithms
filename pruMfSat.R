4e274e6257052ffe9bb16131daf5abc7f1269dd6
JuantBanda <juan.banda31@gmail.com>
2017-03-04 13:17:57 -0600

    Add files via upload

--- pruMfSat.R
+++ pruMfSat.R
@@ -1,4 +1,4 @@
-satw <- scan(file("uuf250-02.cnf"), what = character())
+satw <- scan(file="uf50-01.cnf", what = character())
 nvar=as.numeric(satw[27])
 nk=as.numeric(satw[28])
 satw=as.numeric(satw[29:(nk*4+28)])
@@ -6,6 +6,7 @@ print("num restricciones")
 print(nk)
 print("num de variables")
 print(nvar)
+
 w <- matrix(nrow = nk, ncol = nvar)
 w[,] = 0
 r=1
@@ -24,31 +25,32 @@ mu = vector(mode = "numeric", length=nk)
 g = vector(mode = "numeric", length=nk)
 negations = vector(mode = "numeric", length=nk)
 ### Initial mu vector: ###
-mu[] = 1000
-#print(w)
+mu[] = 0
 mnviol=10000000000
-for(i in 1:10000){
-  m = 1 / (1 + exp(as.vector(-mu%*%w)))+.00000001
+print(mu)
+for(i in 1:100000){
+  m = 1 / (1 + exp(as.vector(1-mu%*%w)))+.00000001
   xb = round(m)
   x=xb
   falsos=which(x==0)
   for(i in 1:length(falsos)){
     x[falsos[i]]=-1
   }
-
   rclau=as.vector(w%*%x)
+  #print(rclau)
   nviol=0
   for(i in 1:nk){
     if(rclau[i]==-3){
       #print("violada")
       nviol=nviol+1
-      mu[i]=abs(mu[i]+1.3)
+      mu[i]=(mu[i]-(1/10^4)*(as.vector(w[i,]%*%x)))
+    #  print(mu[i])
     }
     else{
       #print("satisfecha")
-    #mu[i]=mu[i]+1/10^3
-     mu[i]=abs(mu[i]-.5)
-
+    #m[i]=mu[i]+1/10^3
+     #mu[i]=abs(mu[i]-.5)
+     #mu[i]=(mu[i]+(1/10^10)*abs(as.vector(w[i,]%*%x)))
     }
   #  print(mu)
   }
@@ -62,10 +64,10 @@ for(i in 1:10000){
     mnviol=nviol
     print("Number of violated constraints:")
     print(mnviol)
-    #print(mu)
+  #  print(mu)
     bmu=mu
   }
 
 }
 #print(bmu)
-#print(mu)
+print(mu)
